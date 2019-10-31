f53349465f6cf24bb8629a8cc4e94e101bb3cc9e
Alexandru Dumitrescu <dumitrescu.alexandru95@gmail.com>
2019-10-31 20:25:45 +0200

    Removed unnecessary file

--- test.py
+++
@@ -1,7 +0,0 @@
-from food_truck_env import FoodTruck
-
-ft = FoodTruck()
-print(ft.possible_actions([7,1]))
-possible_future_states = [[7,1] + ft.get_change(act) for act in ft.possible_actions([7, 1])]
-print(possible_future_states)
-print(ft.ft_map)
